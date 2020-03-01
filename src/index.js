f01e1a82586d77a7abcdd7727ff4c2401da1e361
MortyraSky <gornii2014@gmail.com>
2020-03-01 14:07:28 +0300

    feat: add implemet human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number === 0) {
+        return 'zero';
+    }
+    let unitsPlaces = [
+        'zero',
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    ];
+    
+    let decimalPlaces = [
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    
+    let decimalNumbers = [
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+    ];
+    let numbers = "";
+    let hundred = Math.floor((number / 100) % 10);
+    let tens = Math.floor((number % 100) / 10);
+    let units = Math.floor(number % 10);
+
+    if (hundred > 0) {
+        numbers += unitsPlaces[hundred] + ' hundred ';
+    }
+
+    if (tens > 0) {
+        if ((Math.floor(number % 100) > 9) && (Math.floor(number % 100) < 20)) {
+            numbers += decimalNumbers[Math.floor((number % 100) % 10)];
+        } else {
+            numbers += decimalPlaces[tens - 1] + " ";
+        }
+    }
+
+    if (units > 0) {
+        if ((Math.floor(number % 100) > 20) || (Math.floor(number % 100) < 10)) {
+            numbers += unitsPlaces[units];
+        }
+    }
+    return numbers.trim();
 }
